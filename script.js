6a6c18384b0c0bbe24962b00806b22d17e3dce24
Matthew Hayden <matthayden09@gmail.com>
2020-12-17 14:36:16 -0600

    ajax call now works for any city put in the search form

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
     // variables
     var currentDay = moment().format('LL');
-    var searchBtn = $('#searchBtn');
-    var searchCity = $('#searchCity');
+    // var searchBtn = $('#searchBtn');
+    // var searchCity = $('#searchCity');
     var apiKey = '85300d8f018e23e6238ae02a4301aa4d';
-    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=' + apiKey + '&units=imperial'
+    // var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=' + apiKey + '&units=imperial'
     
     // display current date
     $('#currentDay').text(currentDay)
-  
-    // ajax call
-    $.ajax({
-      url: queryURL,
-      method: 'GET'
-    }).then(function(weatherData){
-      console.log(weatherData)
-      // push data to HTML
-      $('#city').text("City: " + weatherData.name)
-      $('#temperature').text("Temperature: " + weatherData.main.temp)
-      $('#humidity').text("Humidity: " + weatherData.main.humidity)
-      $('#windSpeed').text("Wind Speed: " +
-      weatherData.wind.speed)
-    })
-    
-    // attach searchCity input to searchBtn
 
-    // append searchCity input into queryURL
+    $('#searchBtn').on('click', function(event) {
+
+        event.preventDefault();
+
+        // grab the text from the input box
+        var searchCity = $('#searchCity').val();
+        
+        var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchCity + '&appid=' + apiKey + '&units=imperial'
+        
+        $.ajax({
+          url: queryURL,
+          method: 'GET'
+        }).then(function(weatherData){
+          console.log(weatherData)
+          // push data to HTML
+          $('#city').text("City: " + weatherData.name)
+          $('#temperature').text("Temperature: " + weatherData.main.temp)
+          $('#humidity').text("Humidity: " + weatherData.main.humidity)
+          $('#windSpeed').text("Wind Speed: " +
+          weatherData.wind.speed)
+        });
+
+    });
+        
+
       
 
     
